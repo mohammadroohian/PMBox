3c9a489c6ca74efdd15fba1b4dc605e1ba02733b
Mohammad Roohian <Mohammad.rca@gmail.com>
2021-04-30 13:03:30 +0430

    MessageBoxCaller added

---
+++ Assets/PMBox/Scripts/MessageBox/MessageBoxCaller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Pashmak.MessageBox
+{
+    public class MessageBoxCaller : MonoBehaviour
+    {
+        // variable________________________________________________________________
+        [SerializeField] private UnityEvent m_onShowMessage = new UnityEvent();
+
+
+        // property________________________________________________________________
+        public UnityEvent OnShowMessage
+        {
+            get
+            {
+                if (m_onShowMessage == null)
+                    m_onShowMessage = new UnityEvent();
+                return m_onShowMessage;
+            }
+            private set => m_onShowMessage = value;
+        }
+
+
+        // function________________________________________________________________
+        public void ShowMessage(string profileId)
+        {
+            OnShowMessage.Invoke();
+            MessageBoxManager.ShowMessage(profileId);
+        }
+    }
+}
