8d233fb254a242bd9e0360fa6b6b02dc56e279a8
Mohammad Roohian <Mohammad.rca@gmail.com>
2021-04-30 13:03:15 +0430

    MessageBoxTrigger added

---
+++ Assets/PMBox/Scripts/MessageBox/MessageBoxTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Pashmak.MessageBox
+{
+    public class MessageBoxTrigger : MonoBehaviour
+    {
+        // variable________________________________________________________________
+        [SerializeField] private bool m_isActive = true;
+        [SerializeField] private UnityEvent m_onShowMessage = new UnityEvent();
+        [SerializeField] private UnityEvent m_onHideMessage = new UnityEvent();
+
+
+        // property________________________________________________________________
+        public bool IsActive { get => m_isActive; set => m_isActive = value; }
+        public UnityEvent OnShowMessage { get => m_onShowMessage; private set => m_onShowMessage = value; }
+        public UnityEvent OnHideMessage { get => m_onHideMessage; private set => m_onHideMessage = value; }
+
+
+        // monoBehaviour___________________________________________________________
+        void Start()
+        {
+            MessageBoxManager.Instance.m_onShowMessage.AddListener(() =>
+            {
+                if (IsActive)
+                    OnShowMessage.Invoke();
+            });
+            MessageBoxManager.Instance.m_onHideMessage.AddListener(() =>
+            {
+                if (IsActive)
+                    OnHideMessage.Invoke();
+            });
+        }
+    }
+}
